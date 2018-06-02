d4abb39f801551fb4c92650318d5c35bcf38c448
jbub <juraj.bubniak@gmail.com>
2018-06-02 17:03:01 +0200

    Move test utils to fio_test.

--- client_test.go
+++ client_test.go
@@ -1,46 +1,11 @@
 package fio
 
 import (
-	"net/http"
-	"net/http/httptest"
 	"net/url"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-
-var (
-	// mux is the HTTP request multiplexer used with the test server.
-	mux *http.ServeMux
-
-	// client is the GitHub client being tested.
-	client *Client
-
-	// server is a test HTTP server used to provide mock API responses.
-	server *httptest.Server
-
-	// testingToken is token used across all tests.
-	testingToken = "xxxx"
-)
-
-// setup sets up a test HTTP server along with a github.Client that is
-// configured to talk to that test server. Tests should register handlers on
-// mux which provide mock responses for the API method being tested.
-func setup() {
-	// test server
-	mux = http.NewServeMux()
-	server = httptest.NewServer(mux)
-
-	// github client configured to use test server
-	client = NewClient(testingToken, nil)
-	url, _ := url.Parse(server.URL)
-	client.BaseURL = url
-}
-
-// teardown closes the test HTTP server.
-func teardown() {
-	server.Close()
-}
 
 var (
 	sanitizeURLCases = []struct {
---
+++ fio_test.go
@@ -0,0 +1,40 @@
+package fio
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+)
+
+var (
+	// mux is the HTTP request multiplexer used with the test server.
+	mux *http.ServeMux
+
+	// client is the fio client being tested.
+	client *Client
+
+	// server is a test HTTP server used to provide mock API responses.
+	server *httptest.Server
+
+	// testingToken is token used across all tests.
+	testingToken = "xxxx"
+)
+
+// setup sets up a test HTTP server along with a fio.Client that is
+// configured to talk to that test server. Tests should register handlers on
+// mux which provide mock responses for the API method being tested.
+func setup() {
+	// test server
+	mux = http.NewServeMux()
+	server = httptest.NewServer(mux)
+
+	// github client configured to use test server
+	client = NewClient(testingToken, nil)
+	url, _ := url.Parse(server.URL)
+	client.BaseURL = url
+}
+
+// teardown closes the test HTTP server.
+func teardown() {
+	server.Close()
+}
